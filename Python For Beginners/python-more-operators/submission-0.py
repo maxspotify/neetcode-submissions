34c6fec4cae8568ad3d4d004e77022b828ee4650
maxspotify <maxspotify1225@gmail.com>
2026-09-06 17:50:54 -0400

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,14 @@
+a, b, c = 2, 8, 5
+
+my_product = (a*b) // c
+
+my_remainder = (a*b) % c
+
+my_exp = a ** b
+
+my_exp_2 = b ** c
+
+print(my_product)
+print(my_remainder)
+print(my_exp)
+print(my_exp_2)
