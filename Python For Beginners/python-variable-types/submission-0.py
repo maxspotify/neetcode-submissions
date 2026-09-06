b9593c082cfd38019f49e4f7c55dd9a3232c7921
maxspotify <maxspotify1225@gmail.com>
2026-09-06 17:34:51 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 5
+float_type = 5.5
+bool_type = True
+string_type = "this is a string"
+list_type = [1, 2, 3]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
