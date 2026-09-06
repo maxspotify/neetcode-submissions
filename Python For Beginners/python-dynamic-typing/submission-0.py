e1e9367f5c3fd988de38237ae1b27cac294f82f9
maxspotify <maxspotify1225@gmail.com>
2026-09-06 17:35:57 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 5.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "bool"
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
