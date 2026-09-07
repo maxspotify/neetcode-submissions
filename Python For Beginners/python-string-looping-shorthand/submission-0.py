c21ba572677150eb00959a096d9b2ee5c9738ca5
maxspotify <maxspotify1225@gmail.com>
2026-09-07 00:33:12 -0400

    Add: python-string-looping-shorthand - submission-0

---
+++ Python For Beginners/python-string-looping-shorthand/submission-0.py
@@ -0,0 +1,11 @@
+def print_string_characters(word1: str, word2: str) -> None:
+    for letter in word1:
+        print(letter)
+    for letter in word2:
+        print(letter)
+
+
+
+
+# do not modify below this line
+print_string_characters("Hello, World!", "Good Job!")
