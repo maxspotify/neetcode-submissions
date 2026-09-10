738167fb883d87018cc19a24c853fcdc351ca736
maxspotify <maxspotify1225@gmail.com>
2026-09-09 21:25:58 -0400

    Add: python-method-overloading - submission-1

---
+++ Python OOP/python-method-overloading/submission-1.py
@@ -0,0 +1,17 @@
+class TextProcessor:
+    # Implement method overloading for format_text method
+    def format_text(self, *args):
+        if len(args) == 1:
+            return args[0].upper()
+        else:
+            total = ""
+            for arg in args:
+                total += arg
+            return total
+
+
+
+# Don't modify the code below
+processor = TextProcessor()
+print(processor.format_text("hello"))
+print(processor.format_text("hello", "world"))
