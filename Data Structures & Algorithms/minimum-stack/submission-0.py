000531d960f0c20cc3dbb26ddcd5541a30354a59
maxspotify <maxspotify1225@gmail.com>
2026-09-24 18:07:37 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = [] # [(val, min)]
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+            return
+        curr_min = self.stack[-1][1]
+        if val < curr_min:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, curr_min))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+        
