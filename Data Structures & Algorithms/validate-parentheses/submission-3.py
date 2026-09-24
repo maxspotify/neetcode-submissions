00bf4c05febf493377e0ccb19592044429db8e5d
maxspotify <maxspotify1225@gmail.com>
2026-09-23 22:00:11 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {")": "(", "]": "[", "}": "{"}
+        stack = []
+        for char in s:
+            if char in brackets.values():
+                stack.append(char)
+            elif char in brackets:
+                if not stack:
+                    return False
+                last_value = stack.pop()
+                if brackets[char] != last_value:
+                    return False
+        return not bool(stack)
